3daa1a0ac0b4f089ef3975763fa8b872d9236075
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-09 16:37:05 -0700

    initial bmi calculator

---
+++ Lecture demos/bmiCalculator.py
@@ -0,0 +1,12 @@
+'''
+Myrisa Mitchell
+Date: 02/09/22
+
+This program will calculate the BMI of a person. 
+BMI = kg/m**2
+
+Step 1: Prompt user to enter weight and height.
+Step 1a: Convert weight and height values to floats.
+Step 2: Calculate BMI using inputted values.
+Step 3: Print out BMI.
+'''
