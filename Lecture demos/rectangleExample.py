516593b0d4e22ae260c25f893dd3d84fa0b7c500
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-09 16:22:18 -0700

    Initiated steps

--- Lecture demos/rectangleExample.py
+++ Lecture demos/rectangleExample.py
@@ -4,4 +4,10 @@ Date: 02/09/22
 
 This program will prompt for two sides of a rectangle
 and will calculate the area and perimeter of a rectangle.
+
+Step 1: Prompt for side1 and side2 of a rectangle.
+Step 1a: Convert side1 and side2 to float.
+Step 2: Calculate area using side1 and side2. 
+Step 3: Calculate perimeter using side1 and side2. 
+Step 4: Print out area and perimeter.
 '''
