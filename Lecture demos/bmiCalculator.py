9ff67853fcaab9068a5573483b437311af8d8a69
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-09 16:48:55 -0700

    converted to imperial units. final bmi calculator

--- Lecture demos/bmiCalculator.py
+++ Lecture demos/bmiCalculator.py
@@ -12,14 +12,16 @@ Step 3: Print out BMI.
 '''
 
 #step 1
-weight = input("Please enter your weight in kg: ")
-height = input("Please enter your height in meters: ")
+weight = input("Please enter your weight in lbs: ")
+height = input("Please enter your height in inches: ")
 
 #step 1a
 weight = float(weight)
 height = float(height)
 
 #step2
+weight = weight/2.205
+height = height/39.37
 bmi = weight/height**2
 
 #step3
