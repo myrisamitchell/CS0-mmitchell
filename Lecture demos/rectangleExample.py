07fe511c9ce0f48294843c21ac779a15322288a1
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-09 16:31:27 -0700

    completed rectangle example

--- Lecture demos/rectangleExample.py
+++ Lecture demos/rectangleExample.py
@@ -11,3 +11,26 @@ Step 2: Calculate area using side1 and side2.
 Step 3: Calculate perimeter using side1 and side2. 
 Step 4: Print out area and perimeter.
 '''
+
+#step 1
+side1 = input("Please enter one side of the rectangle: ")
+side2 = input("Please enter the other side of the rectangle: ")
+
+#step 1a
+side1 = float(side1)
+side2 = float(side2)
+
+#step 2
+area = side1 * side2
+
+#step 3
+perimeter = (side1*2) + (side2*2)
+
+#step 4
+print("The area of a rectangle with sides",
+    side1, "and", side2,
+    "is", area)
+
+print("The perimeter of a rectangle with sides of",
+    side1, "and", side2,
+    "is", perimeter)
