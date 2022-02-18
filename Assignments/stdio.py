0e2bdf9c0d8e475c91b2b5cf7ec7b14d63c877d6
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-18 11:29:09 -0700

    created stdio project and file with description

---
+++ Assignments/stdio.py
@@ -0,0 +1,18 @@
+'''
+Myrisa Mitchell
+Date: 02/18/22
+
+This program will prompt for the users name, then print out
+the various stages of a hangman game.
+
+Step 1: Prompt for player's name.
+Step 1a: Greet player with their name.
+Step 2: Print out gallows. 
+Step 3: Print hangman's head. 
+Step 4: Print hangman's body.
+Step 5: Print hangman's left arm.
+Step 6: Print hangman's right arm.
+Step 7: Print hangman's left leg.
+Step 8: Print hangmans' right leg.
+
+'''
