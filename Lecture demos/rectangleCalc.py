088d3da878a22b3a9a51751ec8c77947168d1afb
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-21 16:50:40 -0700

    Began rectangle calculator examples with multiple returns

---
+++ Lecture demos/rectangleCalc.py
@@ -0,0 +1,20 @@
+'''
+Name: Myrisa Mitchell
+Date: 2/21/2022
+
+This program will ask for two sides of a rectangle and 
+calculate the area and perimeter
+
+Step 1: Prompt for two sides
+Step 2: Calculate area
+Step 3: Calculate perimeter
+Step 4: Print out area and perimeter
+'''
+
+def main():
+    #Step 1
+    side1 = float(input("Please enter side 1: "))
+    side2 = float(input("Please input side 2: "))
+    pass
+
+main()
