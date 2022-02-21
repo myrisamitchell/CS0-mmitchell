491f1ee1d416e606e61f1b237cc9f33861fd83d3
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-21 16:50:26 -0700

    Added notes to user defined functions

--- Lecture demos/creatingFunctions.py
+++ Lecture demos/creatingFunctions.py
@@ -1,6 +1,9 @@
 #To create a function, use colon to signify that following is 
     #part of the function
 #List within function must be indented
+from audioop import mul
+
+
 def greeting():
     print("Hello Myrisa")  
 
@@ -71,4 +74,52 @@ answer = multiply_nums(42, 5)
 #Step 4
 print("42 * 5 is equal to", answer)
 
+'''
+Global vs. Local variables
+Global: Variable that can be called at any time in terminal
+Local: Variable assigned to a defined function, cannot be called
+    outside of function
+'''
+#To use a global variable in a function:
+def multiply_numbers(num1,num2)
+    global multiply_sum
+    multiply_sum = num1*num2
+    return multiply_sum
+'''multiply_sum can be called outside of the function and the
+program will recognize it
+'''
 
+#Creating a Test Function 
+def tests():
+    assert multiply_nums(2,3) == 6
+    assert multiply_nums(7,8) == 56
+    print(f"All test cases passed")
+
+#Main function
+def main():
+    #To check if inserted function is correct before using it
+    tests()
+    number1 = int(input("Please enter a number: "))
+    number2 = int(input("Please enter another number: "))
+    answer = multiply_nums(number1,number2)
+    print(f"{number1} * {number2} = {answer}")
+
+#Will not do anything until you call it 
+main()
+
+'''
+Instead of testing every time you change code, use assert
+'''
+assert 5 == 5
+#True
+assert 5 == 6 
+#False
+#Using it with a function
+multiply_nums(2,3)
+assert multiply_nums(2,3) == 6
+#True
+assert multiply_nums(2,3) == 7
+#False
+
+#Test functions
+    #See line 92
