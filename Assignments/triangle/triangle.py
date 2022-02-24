34f2e47a758ecaf3cabd678e4de1a04923e9e1f4
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-23 18:14:34 -0700

    program information and steps

---
+++ Assignments/triangle/triangle.py
@@ -0,0 +1,13 @@
+'''
+Name: Myrisa Mitchell
+Date: 2/23/2022
+
+This program will prompt for the three sides of a triangle, 
+calculate the area and perimeter, and print out the area and
+perimeter. 
+
+Step 1: Prompt for sides of triangle.
+Step 2: Calculate area of triangle.
+Step 3: Calculate perimeter of triangle. 
+Step 4: Display area and perimeter of the triangle.
+'''
