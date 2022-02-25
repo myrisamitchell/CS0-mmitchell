c05e1b1367cf7054b3abfd21633f62e628b1f737
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-25 16:50:11 -0700

    created conditionals notes

---
+++ Lecture demos/conditionals.py
@@ -0,0 +1,20 @@
+'''
+Name: Myrisa Mitchell
+Date: 2/25/2022
+'''
+
+def main():
+    x = 5
+    y =10
+    print(f"Before conditional")
+
+    if (x != y):
+        x += 10
+        print("x is not equal to y.")
+        print(f"x = {x}")
+        print(f"We increased x by 10.")
+
+    print(f"After conditional")
+
+
+main()
