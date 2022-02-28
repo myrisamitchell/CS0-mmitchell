71a7ff82762bf4bb421a480ef63f5d0deef259ac
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-02-28 16:46:30 -0700

    added boolean logic example

--- Lecture demos/conditionals.py
+++ Lecture demos/conditionals.py
@@ -3,18 +3,34 @@ Name: Myrisa Mitchell
 Date: 2/25/2022
 '''
 
+"""def main():
+    num = int(input(f"Please enter a whole number: "))
+    
+
+    if (num == 0):
+        print(f"{num} is zero.")
+    elif (num > 0) and (num % 2 == 0):
+        print(f"{num} is even.")
+        print(f"{num} is positive.")
+    elif (num > 0) and (num % 2 ==1):
+        print(f"{num} is odd.")
+        print(f"{num} is positive.")
+    elif (num % 2 == 0):
+        print(f"{num} is even.")
+        print(f"{num} is negative.")
+    else:
+        print(f"{num} is odd.")
+        print(f"{num} is negative.")
+
+main()"""
+
 def main():
-    x = 5
-    y =10
-    print(f"Before conditional")
+    money = int(input(f"How much money do you have in the bank? "))
+    ferrari = input(f"Do you have a ferrari [y/yes | n/no]? ")
 
-    if (x != y):
-        x += 10
-        print("x is not equal to y.")
-        print(f"x = {x}")
-        print(f"We increased x by 10.")
-
-    print(f"After conditional")
-
+    if (money >= 1000000 and (ferrari == "y" or ferrari == "yes")):
+        print("Congrats! You can retire.")
+    else:
+        print("Sorry, back to work.")
 
 main()
