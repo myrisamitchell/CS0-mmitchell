3226b2d5301645c22a336016d6e9ba41ff018011
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-03-04 16:50:35 -0700

    started iterations notes

---
+++ Lecture demos/iterations.py
@@ -0,0 +1,31 @@
+'''
+Name: Myrisa Mitchell
+Date: 03/04/22
+
+This program is to demonstrate loops.
+'''
+
+''' 
+for i in range(10):
+    if (i%2 == 0):
+        continue
+    print(i)
+'''
+'''def main():
+
+    for i in range (10):
+        if (i%2 == 1):
+            continue
+        print(i)
+    else:
+        print("We printed off numbers")
+
+    print("Outside of for loop")
+
+main()'''
+
+def main():
+    n = 13
+    for i in range(2, (n//2)+1):
+
+main()
