456aada1b0361bdcfde8399eefd33f2c345a8452
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-03-05 19:18:32 -0700

    initial commit to functions assignment

---
+++ Assignments/functions/functions.py
@@ -0,0 +1,31 @@
+'''
+Name: Myrisa Mitchell
+Date: 03/05/22
+
+This program will perform arithmetic operations on two given
+numbers prompted from the user.
+
+Step 1: Define a function that sums the two numbers.
+Step 1a: Run test function.
+Step 1b: Print sum. 
+Step 2: Define a function that multiplies two numbers.
+Step 2a: Run test function.
+Step 2b: Print product.
+Step 3: Define a function that divides two numbers.
+Step 3a: Run test function.
+Step 3b: Print quotient.
+Step 4: Define a function that finds the difference of the two numbers.
+Step 4a: Run test function.
+Step 4b: Print the difference.
+Step 5: Define a function that finds the remainder of the first number divided by the second.
+Step 5a: Run test function.
+Step 5b: Print the remainder
+Step 6: Define a function that finds the first number raised to the second number.
+Step 6a: Run test function.
+Step 6b: Print the value.
+Step 7: Define a function that finds the sqrt of both numbers.
+Step 7a: Run test function.
+Step 7b: Print the sqrt.
+Step 8: Promp the user for two numbers.
+Step 9: Call the functions.
+'''
