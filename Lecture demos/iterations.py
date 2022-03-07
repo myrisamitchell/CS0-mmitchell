e22c28a41e925378b09886d639c794b5b9a01abb
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-03-07 16:36:57 -0700

    finished functions to test and print if prime number

--- Lecture demos/iterations.py
+++ Lecture demos/iterations.py
@@ -24,8 +24,33 @@ for i in range(10):
 
 main()'''
 
+def is_prime(number):
+    for i in range(2, ((number//2)+1)):
+        if (number%i == 0):
+            '''print(f"{number} is not prime.")
+            print(f"{number} is divisible by {i}.")'''
+            return False, i
+    else:
+        '''print(f"{n} is prime.")'''
+        return True, 0
+
+def print_result(prime, number, divisor):
+    if not (prime):
+        print(f"{number} is not prime.")
+        print(f"{number} is divisible by {divisor}.")
+    else:
+        print(f"{number} is prime.")
+
+def tests():
+    assert is_prime(42) == (False, 2)
+    assert is_prime(13) == (True, 0)
+    assert is_prime(47) == (True, 0)
+    print("All test cases passed.")
+
 def main():
-    n = 13
-    for i in range(2, (n//2)+1):
+    tests()
+    n = int(input("Please enter a whole number greater than 2: "))
+    prime, divisor = is_prime(n)
+    print_result(prime, n, divisor)
 
 main()
