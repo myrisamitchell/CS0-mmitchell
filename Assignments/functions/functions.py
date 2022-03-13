ef634649309cc103769d78ef315a212fe81bc91c
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-03-12 21:27:20 -0700

    created multiplication function

--- Assignments/functions/functions.py
+++ Assignments/functions/functions.py
@@ -6,38 +6,39 @@ This program will perform arithmetic operations on two given
 numbers prompted from the user.
 
 Step 1: Define a function that sums the two numbers.
-Step 1a: Run test function.
-Step 1b: Print sum. 
+Step 1a: Print sum. 
 Step 2: Define a function that multiplies two numbers.
-Step 2a: Run test function.
-Step 2b: Print product.
+Step 2a: Print product.
 Step 3: Define a function that divides two numbers.
-Step 3a: Run test function.
-Step 3b: Print quotient.
+Step 3a: Print quotient.
 Step 4: Define a function that finds the difference of the two numbers.
-Step 4a: Run test function.
-Step 4b: Print the difference.
+Step 4a: Print the difference.
 Step 5: Define a function that finds the remainder of the first number divided by the second.
-Step 5a: Run test function.
-Step 5b: Print the remainder
+Step 5a: Print the remainder
 Step 6: Define a function that finds the first number raised to the second number.
-Step 6a: Run test function.
-Step 6b: Print the value.
+Step 6a: Print the value.
 Step 7: Define a function that finds the sqrt of both numbers.
-Step 7a: Run test function.
-Step 7b: Print the sqrt.
+Step 7a: Print the sqrt.
 Step 8: Promp the user for two numbers.
-Step 9: Call the functions.
+Step 9: Call test functions and main function.
 '''
 
 #Step 1
 def find_sum(num1, num2):
     sum_answer = num1 + num2
-    return sum_answer
-    #step1b
+    #Step 1a
     print(f"{num1} + {num2} = {sum_answer}.")
+    return sum_answer
+
+#Step 2
+def find_product(num1, num2):
+    prod_answer = num1 * num2
+    #Step 2a
+    print(f"The product of {num1} and {num2} is {prod_answer}.")
+    return prod_answer
 
 
+    
     
 
 def main():
