f7290c9a34dd5450fd9948edaf23c40930ea8668
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-03-20 09:18:06 -0500

    added program information

---
+++ Assignments/conditionals/conditional.py
@@ -0,0 +1,18 @@
+'''
+Name: Myrisa Mitchell
+Date: March 20, 2022
+
+This program will compute the sum, product, max, min, and average
+of 5 numbers prompted from the user. 
+
+Step 1: Define a function that takes 5 numbers and calculates the sum
+Step 2: Define a function that takes 5 numbers and calculates the product
+Step 3: Define a function that takes 5 numbers and calculates the average
+Step 4: Define a function that takes 5 numbers and finds the maximum 
+Step 5: Define a function that takes 5 numbers and finds the minimum
+Step 6: Define a main function
+Step 6a: Prompt users for 5 numbers
+Step 6b: Call all functions and displays results
+Step 7: Define a test function that tests each function twice
+
+'''
