5c81bd517cd38506de5bbdcf19c31e7898971a99
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-03-28 16:52:06 -0600

    strings lecture notes

--- Lecture demos/strings.py
+++ Lecture demos/strings.py
@@ -1,7 +1,37 @@
+# def main():
+#     s = "Pirates of the Carribean"
+#     # split_string = s.split()
+#     for i in range (len(s)):
+#         print(s[i])
+
+# main()
+
+
+
 def main():
-    s = "Pirates of the Carribean"
-    # split_string = s.split()
-    for i in range (len(s)):
-        print(s[i])
+    str_string = "The next Tuesday is tomorrow"
+    #prints out whole string
+    #To print in reverse: [ : :-1]
+    print(str_string[ : :])
+
+    #String conditionals
+    #print("Tuesday" in str_string)
+    #Checks if the string is in the original string and returns True
+
 
 main()
+
+import string
+
+def main2():
+    str_string = "The next Tuesday is tomorrow"
+    lower_string = ""
+    for ch in str_string:
+        if ch in string.ascii_uppercase:
+            ch = ch.lower()
+        elif ch in string.whitespace:
+            ch = "-"
+        lower_string += ch
+    print(lower_string)
+
+main2()
