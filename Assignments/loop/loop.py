6cd7d2ecf045eda46217a5c291060ce72d53231d
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-01 12:03:39 -0600

    Steps 1-6 completed and tested manually multiple times. Final submission.

--- Assignments/loop/loop.py
+++ Assignments/loop/loop.py
@@ -16,3 +16,67 @@ Step 5c: If the play doesn't guess within 6 tries, they lose the game
 Step 6: Once game is over, inform player whether they won or lost 
 Step 6a: Reveal the number.
 '''
+
+import random 
+
+#Step 1
+def input_name():
+    print(f"Welcome to the -- Guess the Number -- game!")
+    firstname = input("What is your name? ")
+    return firstname
+    
+#Step 2
+def greet_player(first_name):
+    print(f"Hello, {first_name}. I am thinking of a whole number between 1 and 20.")
+    print(f"You get 6 tries to guess the number.")
+
+def guess_number():
+    #Step 3
+    number = random.randint(1,20)
+    #Step 4
+    guesses = 0
+    #Step 5
+    while (guesses < 6):
+        guesses += 1
+        guess = int(input("Take a guess: "))
+        #Step 5a
+        if (guess > number):
+            print(f"That guess is too high. Try again.")
+            continue
+        elif (guess < number): 
+            print("That guess is too low. Try again.")
+            continue
+        else:
+            #Step 5b
+            print("Congratulations! You've guessed the number.")
+            break
+    else: 
+        #Step 5c
+        print("Sorry, you've taken too many tries to guess the number.")
+
+    #Step 6
+    if (guess == number):
+        if (guesses == 1):
+            print(f"Well done! You guessed my number in {guesses} try!")
+        else:
+            print(f"Well done! You guessed my number in {guesses} tries!")
+    else:
+        #Step 6a
+        print(f"My number was {number}. Maybe next time!")
+
+def play_again():
+    again = input("Would you like to play again? [Enter y/n]: ")
+    if (again == "y"):
+        guess_number()
+        play_again()
+    else:
+        print(f"Okay, see you next time!")
+
+def main():
+    first_name = input_name()
+    greet_player(first_name)
+    guess_number()
+    play_again()
+        
+if __name__ == "__main__":
+    main()
