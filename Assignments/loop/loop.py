1b096fa8e6542c33726e006af60c692de37008ec
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-01 11:04:53 -0600

    Created loop assignment and added program info

---
+++ Assignments/loop/loop.py
@@ -0,0 +1,18 @@
+'''
+Name: Myrisa Mitchell
+Date: 04/01/22
+
+This program will prompt a user to guess a randomly selected number until
+they get it correctly.
+
+Step 1: Ask for player's name
+Step 2: Greet player
+Step 3: Generate random number between 1-20
+Step 4: Ask player to guess the random number
+Step 5: Have player repeat guess until they get it
+Step 5a: Inform player if their guess is too high or too low
+Step 5b: If player guesses within 6 tries, they win the game
+Step 5c: If the play doesn't guess within 6 tries, they lose the game
+Step 6: Once game is over, inform player whether they won or lost 
+Step 6a: Reveal the number.
+'''
