c6f5b9e464fca48bf19f07aac74454a24c9f8a40
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-07 13:08:36 -0600

    created avion assignment

---
+++ Assignments/avion/avion.py
@@ -0,0 +1,13 @@
+'''
+Name: Myrisa Mitchell
+Date: 04/07/22
+
+This program will look through 5 rows of "blimps" to find CIA blimps that contain 
+"FBI" and will output the row number that the CIA blimp
+
+Step 1: Search through each row of blimps in first output
+Step 2: If a row contains the string "FBI", print out row number
+Step 3: If no rows in the input contain "FBI", print out "He got away!"
+Step 4: Repeat for each input of blimps. 
+'''
+
