82f92364c791fe8b8c0bab916af5dadb98264d93
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-07 14:34:27 -0600

    attempt to include test cases

--- Assignments/avion/avion.py
+++ Assignments/avion/avion.py
@@ -12,25 +12,27 @@ Step 4: Create test function with 3 test cases
 '''
 
 def tests():
-    pass
+    assert search_blimps(FBI-12, HAND-97, FOURTH3, HAR-35, HIFBI-7) == [1, 5]
 
 
 def search_blimps(blimps):
     cia_blimp = []
     #Step 1
-    for i in range(len(blimps)):
+    for i in range(5):
         if ("FBI" in blimps[i]):
             cia_blimp.append(i+1)
     #Step 2
     if cia_blimp:
-        print(cia_blimp, sep = " ")
+        #Source: https://github.com/robertusbagaskara/kattis-solutions/blob/master/source/Avion/avion.py
+        for i in range(len(cia_blimp)):
+            print('%i ' % cia_blimp[i], end = '')
     else: 
         #Step 3
         print("HE GOT AWAY!")
 
 def main():
-    input_blimp = input("Enter 5 elements of code separated by a space: ")
-    blimps = input_blimp.split()
+    tests()
+    blimps = input()
     search_blimps(blimps)
 
 if __name__ == "__main__":
