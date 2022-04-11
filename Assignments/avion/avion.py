eee09a9c50a28c2f8413bbe6c0bf36b4582f13ad
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-11 12:27:33 -0600

    avion final submission

--- Assignments/avion/avion.py
+++ Assignments/avion/avion.py
@@ -12,28 +12,33 @@ Step 4: Create test function with 3 test cases
 '''
 
 def tests():
-    assert search_blimps(FBI-12, HAND-97, FOURTH3, HAR-35, HIFBI-7) == [1, 5]
+    assert search_blimps("FBI-12", "HAND-97", "FOURTH3", "HAR35", "HIFBI-7") == [1, 5]
+    assert search_blimps("HI-34T", "FNI-J2", "FBI-7", "CRIFBI-3", "HRG43H") == [3, 4]
+    assert search_blimps("CRY-Y3", "HE-3HA", "AH75H3", "HE-6HTE", "MARIKA-8") == []  
 
 
-def search_blimps(blimps):
+def search_blimps(blimp1, blimp2, blimp3, blimp4, blimp5):
+    blimps = [blimp1, blimp2, blimp3, blimp4, blimp5]
     cia_blimp = []
     #Step 1
     for i in range(5):
         if ("FBI" in blimps[i]):
             cia_blimp.append(i+1)
-    #Step 2
-    if cia_blimp:
-        #Source: https://github.com/robertusbagaskara/kattis-solutions/blob/master/source/Avion/avion.py
-        for i in range(len(cia_blimp)):
-            print('%i ' % cia_blimp[i], end = '')
-    else: 
-        #Step 3
-        print("HE GOT AWAY!")
+    return cia_blimp
 
 def main():
     tests()
-    blimps = input()
-    search_blimps(blimps)
+
+    blimp1 = input()
+    blimp2 = input()
+    blimp3 = input()
+    blimp4 = input()
+    blimp5 = input()
+    ciaBlimp = search_blimps(blimp1, blimp2, blimp3, blimp4, blimp5)
+    if not ciaBlimp:
+        print("HE GOT AWAY!")
+    else:
+        print(*ciaBlimp)
 
 if __name__ == "__main__":
     main()
