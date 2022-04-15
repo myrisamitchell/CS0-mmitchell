8ca38dffe9e1eb4b85a5a32c05a03c10ed409d89
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-15 10:35:08 -0600

    created assignment and added programming info

---
+++ Assignments/simon_says/simonsays.py
@@ -0,0 +1,12 @@
+'''
+Name: Myrisa Mitchell
+Date: April 15, 2022
+
+This program will search through rows of commands and do what simon says in strings that 
+contain "Simon says"
+
+Step 1: Take in input of strings
+Step 2: Search strings for "Simon says"
+Step 3: Print out end of strings that contain "Simon says"
+Step 4: Write test function that contains three test cases
+'''
