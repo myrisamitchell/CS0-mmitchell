7ae5c209761c786eaafe3c19ea5b39909006d828
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-20 16:23:01 -0600

    improved after lecture

--- Assignments/loop/loop.py
+++ Assignments/loop/loop.py
@@ -31,12 +31,15 @@ def greet_player(first_name):
     print(f"You get 6 tries to guess the number.")
 
 def guess_number():
+    games_won = 0
+    games_lost = 0
     #Step 3
     number = random.randint(1,20)
     #Step 4
     guesses = 0
+    guessed = False
     #Step 5
-    while (guesses < 6):
+    while (guesses < 6 and not guessed):
         guesses += 1
         guess = int(input("Take a guess: "))
         #Step 5a
@@ -49,34 +52,47 @@ def guess_number():
         else:
             #Step 5b
             print("Congratulations! You've guessed the number.")
-            break
+            guessed = True
     else: 
         #Step 5c
         print("Sorry, you've taken too many tries to guess the number.")
 
     #Step 6
     if (guess == number):
+        games_won += 1
         if (guesses == 1):
             print(f"Well done! You guessed my number in {guesses} try!")
         else:
             print(f"Well done! You guessed my number in {guesses} tries!")
     else:
+        games_lost += 1
         #Step 6a
         print(f"My number was {number}. Maybe next time!")
+    return games_won, games_lost
 
 def play_again():
     again = input("Would you like to play again? [Enter y/n]: ")
     if (again == "y"):
-        guess_number()
-        play_again()
-    else:
-        print(f"Okay, see you next time!")
+        return True
+    return False
+    #     guess_number()
+    #     play_again()
+    # else:
+    #     print(f"Okay, see you next time!")
 
 def main():
+    runagain = True 
     first_name = input_name()
     greet_player(first_name)
-    guess_number()
-    play_again()
+    while (runagain):
+        guess_number()
+        play_again()
+        if play_again() == False:
+            runagain = False
+
+    games_stats = guess_number()
+    print(f"You won {games_stats[0]} games.")
+    print(f"You lost {games_stats[1]}.")
         
 if __name__ == "__main__":
     main()
