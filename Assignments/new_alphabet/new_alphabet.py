7cf6f69161de622a4d4910df4f494c888cc5ee80
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-04-25 16:16:33 -0600

    created new_alphabet assignment

---
+++ Assignments/new_alphabet/new_alphabet.py
@@ -0,0 +1,11 @@
+'''
+Name: Myrisa Mitchell
+Date: April 25, 2022
+
+This program will convert letters of the alphabet to a "New alphabet".
+
+Step 1: Create dictionary of new alphabet
+Step 2: Take input of string and convert to new alphabet
+Step 3: Print out string with new alphabet
+Step 4: Create test function with 3 test cases
+'''
